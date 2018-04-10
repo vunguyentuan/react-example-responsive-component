dff5c20a033c76da2157cdc10771d83698b95310
Vu Nguyen <tuanvu.vn007@gmail.com>
2018-04-10 15:54:13 +0700

    create new parentsizeobserve component to handle size change

--- src/components/Responsive/index.js
+++ src/components/Responsive/index.js
@@ -1,37 +1,31 @@
 import React, { Component } from 'react';
-import debounce from 'lodash/debounce'
+import ParentSizeObserve from '../ParentSizeObserve'
 
 class Responsive extends Component {
   static defaultProps = {
     breakpoint: 300
   }
-
-  state = {}
-
-  componentDidMount() {
-    this.setState({
-      containerWidth: this._element.clientWidth
-    })
-
-    window.addEventListener('resize', this.handleWindowResize)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleWindowResize)
-  }
-
-  handleWindowResize = debounce(() => {
-    this.setState({
-      containerWidth: this._element.clientWidth
-    })
-  }, 100)
   
   render() {
     const { breakpoint, layout1: LayoutA, layout2: LayoutB } = this.props;
     return (
-      <div className="myApp" ref={node => this._element = node}>
-        {this.state.containerWidth && this.state.containerWidth <= breakpoint && <LayoutA />}
-        {this.state.containerWidth && this.state.containerWidth > breakpoint && <LayoutB />}
+      <div>
+        <ParentSizeObserve>
+          {( size ) => (
+            <div>
+              {size <= breakpoint && <LayoutA />}
+              {size > breakpoint && <LayoutB />}
+            </div>
+          )}
+        </ParentSizeObserve>
+
+        <ParentSizeObserve>
+          {( size ) => (
+            <div style={{height: 300, backgroundColor: size > 300 ? 'red': 'yellow'}}>
+              content
+            </div>
+          )}
+        </ParentSizeObserve>
       </div>
     );
   }
